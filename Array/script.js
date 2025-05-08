1c26dfee0c978f25a5cafe61b9d19194486d0381
ADEOLUWA AYODELE YETUNDE <gracilar16@gmail.com>
2025-05-08 16:56:46 +0100

    Add files via upload

---
+++ Array/script.js
@@ -0,0 +1,13 @@
+let nameBox=['ife', 'seun', 'david', 'ayo', 'charles']
+console.log(nameBox)
+console.log(nameBox.length)
+console.log(nameBox.push('adunni'))
+console.log(nameBox)
+
+/*let scoreBoard= [1,2,3,4,5]
+let studentData={name:'ife', score:'100'}*/
+
+let scoreBoard= [1,2,3,4,5,6,{Name: 'ife', score: '60'}]
+console.log(scoreBoard[6].Name)
+
+console.log(scoreBoard[6].score)
